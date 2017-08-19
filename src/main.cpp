354d04aa8545f30ae368739f8bc32b2a4cbdeaee
bear <xwpcom@163.com>
2017-08-19 17:23:32 +0800

    增加了vs2015工程，方便编辑代码

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 	#include "serial.h"
 }
 #include "s3c24xx.h"
-
+#include "base.h"
 
 
 #define	GPFCON		(*(volatile unsigned long *)0x56000050)
@@ -23,6 +23,8 @@ void  wait(volatile unsigned long dly)
 //仅供测试使用,2440开发板内存64MB,足够大了
 void *operator new(unsigned int bytes)
 {
+	puts("call new\r\n");
+
 	static unsigned char *p= (unsigned char*)(0x30000000 + 1024 * 256);
 	unsigned char *addr = p;
 	p += bytes;
@@ -39,62 +41,59 @@ void *operator new(unsigned int bytes)
 void operator delete(void *p)
 {
 	//do nothing
+	puts("call delete\r\n");
 }
 
-class Base
+
+class Main
 {
 public:
-	Base()
-	{
-		puts("Base::Base()\r\n");
-	}	
-	~Base()
-	{
-		puts("Base::~Base()\r\n");
-	}	
+	static int main();
 };
 
-extern "C" int main()
+int Main::main()
 {
-	
-    uart0_init();   // 波特率115200，8N1(8个数据位，无校验位，1个停止位)
+	uart0_init();   // 波特率115200，8N1(8个数据位，无校验位，1个停止位)
 
-	puts("\r\nHello from uboot\r\n");
-/*
-    while(1)
-    {
-        // 从串口接收数据后，判断其是否数字或子母，若是则加1后输出
-	    unsigned char c = getc();
-        //if (isDigit(c) || isLetter(c))
-        if(c==0x0D)
-        {
-        	puts("\r\n");
-        }
-        else
-    	{
-	        putc(c);
-    	}
-    }
-    //*/
+	puts("\r\nHello from uboot,vs2015#1\r\n");
+	/*
+	while(1)
+	{
+	// 从串口接收数据后，判断其是否数字或子母，若是则加1后输出
+	unsigned char c = getc();
+	//if (isDigit(c) || isLetter(c))
+	if(c==0x0D)
+	{
+	puts("\r\n");
+	}
+	else
+	{
+	putc(c);
+	}
+	}
+	//*/
 
 	{
-		Base *b=new Base();
+		Base *b = new Base();
 		delete b;
 	}
-	
+
 	unsigned long i = 0;
 
-	GPFCON = GPF4_out|GPF5_out|GPF6_out;		// 将LED1,2,4对应的GPF4/5/6三个引脚设为输出
+	GPFCON = GPF4_out | GPF5_out | GPF6_out;		// 将LED1,2,4对应的GPF4/5/6三个引脚设为输出
 
-	while(1){
+	while (1) {
 		wait(30000);
-		GPFDAT = (~(i<<4));	 	// 根据i的值，点亮LED1,2,4
-		if(++i == 8)
+		GPFDAT = (~(i << 4));	 	// 根据i的值，点亮LED1,2,4
+		if (++i == 8)
 			i = 0;
 	}
 
-
-
 	return 0;
+}
+
+extern "C" int main()
+{
+	return Main::main();
 }
 
